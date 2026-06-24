bc2640dbb07281b02d1adb7d02265e4f7cb42cc0
Ishwarya-1234 <ishwaryark6@gmail.com>
2026-06-24 22:50:42 +0530

    Add solution for LeetCode  find duplicate

---
+++ Arrays/LC287_Find_Duplicate.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& arr) {
+
+        int slow = arr[0];
+        int fast = arr[0];
+
+        do {
+            slow = arr[slow];           // +1 step
+            fast = arr[arr[fast]];      // +2 steps
+        } while (slow != fast);
+
+        slow = arr[0];
+
+        while (slow != fast) {
+            slow = arr[slow];           // +1 step
+            fast = arr[fast];           // +1 step
+        }
+
+        return slow;
+    }
+};
